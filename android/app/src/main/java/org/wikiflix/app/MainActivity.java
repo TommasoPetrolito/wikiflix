2c6b5f4e9f9497b5b39f45227084247112c7a7f2
Tommaso Petrolito <tommasouni@gmail.com>
2026-01-06 15:54:46 +0100

    alpha version of the web-app

--- android/app/src/main/java/org/wikiflix/app/MainActivity.java
+++ android/app/src/main/java/org/wikiflix/app/MainActivity.java
@@ -1,5 +1,20 @@
 package org.wikiflix.app;
 
+import android.os.Bundle;
+import androidx.core.splashscreen.SplashScreen;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    // Handles the splash screen transition.
+    SplashScreen.installSplashScreen(this);
+
+    super.onCreate(savedInstanceState);
+
+    // Enables edge-to-edge display from the Android side.
+    // This works with the CSS safe-area-inset variables from the web side.
+    WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+  }
+}
